509096008141728c26371a8ee90bb2b90fe8d721
Beli90 <andi.blubb@hotmail.de>
2016-05-24 13:51:15 +0200

    added other projects

---
+++ Project BeSe-android/src/com/beli/projectbese/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.beli.projectbese;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.beli.projectbese.ProjectBeSe;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectBeSe(), config);
+	}
+}
